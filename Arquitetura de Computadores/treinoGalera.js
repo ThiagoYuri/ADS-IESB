5d18b7d5ed45afe900400b8187e92881734006a1
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-07-29 09:28:02 -0300

    Arquitetura de computadores

---
+++ Arquitetura de Computadores/treinoGalera.js
@@ -0,0 +1,28 @@
+a = (18 > 12)
+b = (18 > 20)
+
+
+
+a = true
+b = false
+
+// && == AND
+// || == OR
+
+
+
+if (a && b) {
+  alert("AND passou")
+}
+
+if (a || b) {
+  alert("OR passou")
+}
+
+// ! == NOT
+if (!(a && b)) {
+ alert("AND com NOT passou")
+}
+
+
+
